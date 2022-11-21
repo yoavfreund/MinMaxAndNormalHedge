8b38552f91b4fd328761041c26afa1f8be4faa5f
Yoav Freund <yfreund@ucsd.edu>
2022-11-20 18:05:30 -0800

    Working on adversary prefers small steps

---
+++ auto/macros.el
@@ -0,0 +1,85 @@
+(TeX-add-style-hook
+ "macros"
+ (lambda ()
+   (TeX-run-style-hooks
+    "amsmath"
+    "bm")
+   (TeX-add-symbols
+    '("atII" 1)
+    '("atI" 1)
+    '("att" 1)
+    '("at" 1)
+    '("lowerscore" 1)
+    '("upperscore" 1)
+    '("Klat" 1)
+    '("Ilat" 1)
+    '("Tset" 1)
+    '("finalPot" 1)
+    '("SP" 1)
+    '("P" 2)
+    '("E" 2)
+    '("newmcommandiii" 2)
+    '("newmcommandii" 2)
+    '("newmcommandi" 2)
+    '("newmcommand" 2)
+    '("notem" 1)
+    '("note" 1)
+    "figline"
+    "D"
+    "reals"
+    "argmin"
+    "argmax"
+    "R"
+    "vR"
+    "state"
+    "pot"
+    "potPQ"
+    "potLA"
+    "finalPotT"
+    "finalPotR"
+    "upperpot"
+    "upperpotb"
+    "upperpotd"
+    "upperpotMdk"
+    "upperpotMdj"
+    "lowerpot"
+    "lowerpotb"
+    "lowerpotd"
+    "lowerpotj"
+    "lowerpotMdk"
+    "lowerpoteven"
+    "realT"
+    "score"
+    "upperscoreM"
+    "upperscoreMd"
+    "upperscoreMdk"
+    "lowerscoreM"
+    "lowerscoreMd"
+    "learnerM"
+    "learnerMb"
+    "learnerMd"
+    "learnerMdk"
+    "learnerMdj"
+    "legalLearner"
+    "adversM"
+    "adversMb"
+    "adversMd"
+    "adversMdk"
+    "agloss"
+    "Bias"
+    "deltat"
+    "diffweight"
+    "upperpotdiff"
+    "Binom"
+    "radsum"
+    "var"
+    "V")
+   (LaTeX-add-environments
+    "lemma"
+    "claim"
+    "theorem"
+    "corollary"
+    "definition"
+    "question"))
+ :latex)
+
